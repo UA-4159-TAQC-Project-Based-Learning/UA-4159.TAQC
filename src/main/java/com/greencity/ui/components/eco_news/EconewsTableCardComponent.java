211ef6d7e13d5d41b006dbc9dd1aebfb8e2967c5
OlhaS <oshesternyak@gmail.com>
2025-11-11 23:06:16 -0500

    add EconewsTableCardComponent

---
+++ src/main/java/com/greencity/ui/components/eco_news/EconewsTableCardComponent.java
@@ -0,0 +1,12 @@
+package com.greencity.ui.components.eco_news;
+
+import com.greencity.ui.components.BaseComponent;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class EconewsTableCardComponent extends BaseComponent {
+
+    public EconewsTableCardComponent(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+}
