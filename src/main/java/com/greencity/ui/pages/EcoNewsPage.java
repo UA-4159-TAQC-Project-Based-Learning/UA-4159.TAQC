3d03e56d70a11df8e22c05d8816db611b91c1504
Karyna Shevchenko <shevchenkoks1810@gmail.com>
2025-11-13 15:50:21 +0000

    updated selectors

--- src/main/java/com/greencity/ui/pages/EcoNewsPage.java
+++ src/main/java/com/greencity/ui/pages/EcoNewsPage.java
@@ -5,14 +5,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class EcoNewsPage extends BasePage{
+public class EcoNewsPage extends BasePage {
 
     @Getter
     @FindBy(css = "h1.main-header")
     private WebElement mainHeader;
 
     @Getter
-    @FindBy(css = "span.search-img")
+    @FindBy(css = "div.container-img:has(.search-img)")
     private WebElement searchButton;
 
     @Getter
@@ -24,11 +24,11 @@ public class EcoNewsPage extends BasePage{
     private WebElement cancelSearchButton;
 
     @Getter
-    @FindBy(css = "span.bookmark-img")
+    @FindBy(css = "div.container-img:has(.bookmark-img)")
     private WebElement bookmarkButton;
 
     @Getter
-    @FindBy(css = "div.container-img")
+    @FindBy(css = "div.container-img:has(.my-events-img)")
     private WebElement myEventsButton;
 
     @Getter
