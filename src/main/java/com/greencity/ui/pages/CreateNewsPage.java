ad2cb556f364ab91da6d2b0897e1e303608d6a62
elena <rudneva7elena@gmail.com>
2025-11-20 23:23:39 +0100

    just commit

--- src/main/java/com/greencity/ui/pages/CreateNewsPage.java
+++ src/main/java/com/greencity/ui/pages/CreateNewsPage.java
@@ -1,10 +1,49 @@
 package com.greencity.ui.pages;
 
+import com.greencity.ui.components.createNews.AddImageComponent;
+import com.greencity.ui.components.createNews.CreateNewsButtonsComponent;
+import com.greencity.ui.components.createNews.CreateNewsInputComponent;
+import com.greencity.ui.components.createNews.NewsTagsComponent;
+import lombok.Getter;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class CreateNewsPage extends BasePage {
 
+    @Getter
+    private final CreateNewsInputComponent createNewsInputComponent;
+    @Getter
+    private final AddImageComponent addImageComponent;
+    @Getter
+    private final NewsTagsComponent newsTagsComponent;
+    @Getter
+    private final CreateNewsButtonsComponent createNewsButtonsComponent;
+    @Getter
+    @FindBy(css = "h2.title-header")
+    private WebElement pageTitle;
+    @Getter
+    @FindBy(css = "p.title-description")
+    private WebElement pageTitleDescription;
+    @Getter
+    @FindBy(css = "div.form-container")
+    private WebElement inputsComponentRoot;
+    @Getter
+    @FindBy(css = "div.image-block")
+    private WebElement addImageComponentRoot;
+    @Getter
+    @FindBy(css = "div.tags-block")
+    private WebElement newsTagsComponentRoot;
+    @Getter
+    @FindBy(css = "div.submit-buttons")
+    private WebElement submitButtonsRoot;
+
     public CreateNewsPage(WebDriver driver) {
         super(driver);
+        this.createNewsInputComponent = new CreateNewsInputComponent(driver, inputsComponentRoot);
+        this.addImageComponent = new AddImageComponent(driver, addImageComponentRoot);
+        this.newsTagsComponent = new NewsTagsComponent(driver, newsTagsComponentRoot);
+        this.createNewsButtonsComponent = new CreateNewsButtonsComponent(driver, submitButtonsRoot);
     }
+
 }
