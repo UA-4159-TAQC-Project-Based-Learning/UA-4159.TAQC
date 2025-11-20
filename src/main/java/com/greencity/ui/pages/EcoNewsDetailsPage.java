7ea60b1be7dcc59dc4dd1c9de1d58c9bada2231f
OlhaS <oshesternyak@gmail.com>
2025-11-19 22:43:09 -0500

    add elements into EcoNewsDetailsPage

--- src/main/java/com/greencity/ui/pages/EcoNewsDetailsPage.java
+++ src/main/java/com/greencity/ui/pages/EcoNewsDetailsPage.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
+import java.util.List;
 
 public class EcoNewsDetailsPage extends BasePage {
 
@@ -16,6 +16,33 @@ public class EcoNewsDetailsPage extends BasePage {
     @FindBy(css = "div.edit-news")
     private WebElement editNewsButton;
 
+    @Getter
+    @FindBy(xpath = "//div[@class='back-button']/a")
+    private WebElement backButton;
+
+    @Getter
+    @FindBy(xpath = "//div[@class='tags']/div")
+    private List<WebElement> allTagsElements;
+
+    @Getter
+    @FindBy(css = "div.news-title")
+    private WebElement titleElement;
+
+    @Getter
+    @FindBy(css = "div.news-info-date")
+    private WebElement dateInfoElement;
+
+    @Getter
+    @FindBy(css = "div.news-info-author")
+    private WebElement authorInfoElement;
+
+    @Getter
+    @FindBy(css = "img.news_like")
+    private WebElement likeImgElement;
+
+    @Getter
+    @FindBy(css = "span.numerosity_likes")
+    private WebElement numbersOfLikesElement;
 
     public EcoNewsDetailsPage(WebDriver driver) {
         super(driver);
@@ -27,5 +54,27 @@ public class EcoNewsDetailsPage extends BasePage {
         editNewsButton.click();
         return new EditEcoNewsPage(driver);
     }
+
+    public EcoNewsPage clickBackButton() {
+        this.backButton.click();
+        return new EcoNewsPage(driver);
+    }
+
+    public String getAuthorName(){
+        return authorInfoElement.getText();
+    }
+
+    public Integer getNumberOfLikes() {
+        return Integer.parseInt(numbersOfLikesElement.getText());
+    }
+
+    public String getTitle(){
+        return titleElement.getText();
+    }
+
+    public String getDateInfo(){
+        return dateInfoElement.getText();
+    }
+
 
 }
