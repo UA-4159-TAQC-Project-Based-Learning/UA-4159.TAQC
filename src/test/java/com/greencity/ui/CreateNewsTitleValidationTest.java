d444eac40301f723534f3589059118024dec25fd
yevhenmo <mo.music.mo@gmail.com>
2025-11-25 11:59:02 +0200

    add one full test

--- src/test/java/com/greencity/ui/CreateNewsTitleValidationTest.java
+++ src/test/java/com/greencity/ui/CreateNewsTitleValidationTest.java
@@ -6,6 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class CreateNewsTitleValidationTest extends TestRunnerWithUser {
 
@@ -31,18 +32,35 @@ public class CreateNewsTitleValidationTest extends TestRunnerWithUser {
         }
     }
 
-    @Test(description = "Title input should be invalid when empty")
-    public void testTitleEmpty() {
-        Assert.assertTrue(
-                createNewsPage.getTitleInput()
-                        .typeText("")
-                        .clear()
-                        .isInvalid(),
-                "Expected title input has ng-invalid class tag"
-        );
+    @Test(description = "Title input invalid when empty and Publish disabled")
+    public void testTitleEmptyAndPublishDisabled() {
+        SoftAssert softAssert = new SoftAssert();
+
+        boolean isInvalid = createNewsPage.getTitleInput()
+                .clickOnField()
+                .typeText("")
+                .clear()
+                .isInvalid();
+        softAssert.assertTrue(isInvalid,
+                "Expected title input has ng-invalid class tag when field is empty");
+
+        boolean publishEnabled = createNewsPage.getCreateNewsButtonsComponent()
+                .isPublishEnabled();
+        softAssert.assertFalse(publishEnabled,
+                "Expected Publish button to be disabled when title is empty");
+
+        boolean fieldInfoMessage = createNewsPage.getTitleInput()
+                .getFieldInfoText()
+                .trim()
+                .equalsIgnoreCase("0/170");
+        softAssert.assertTrue(fieldInfoMessage,
+                "Expected message must be 0/170");
+
+        softAssert.assertAll();
     }
 
     @Test(description = "Title input should not exceed 170 characters")
+    //in testcase requirement max length = 170 char functional on site implemented max length = 171
     public void testTitleMaxLength() {
         String longTitle = "A".repeat(200);
 
