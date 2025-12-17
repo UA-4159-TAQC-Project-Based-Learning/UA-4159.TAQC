80a62f04a8abe1312aa8feb358f83bbdba486b95
yevhenmo <mo.music.mo@gmail.com>
2025-12-17 20:53:33 +0200

    fix steps

--- src/test/java/com/greencity/cucumber/steps/CreateNewsStep.java
+++ src/test/java/com/greencity/cucumber/steps/CreateNewsStep.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebElement;
 
 public class CreateNewsStep {
     private Hooks hooks;
+    private CreateNewsPage createNewsPage;
 
     public CreateNewsStep(Hooks hooks) {
         this.hooks = hooks;
@@ -18,7 +19,7 @@ public class CreateNewsStep {
     public void the_user_opens_the_create_news_page() {
         hooks.homePage = new HomePage(hooks.getDriver());
         hooks.homePage.refreshPage();
-        hooks.createNewsPage = hooks.homePage
+        createNewsPage = hooks.homePage
                 .getHeader()
                 .clickEcoNewsNavItem()
                 .clickCreateNews();
@@ -26,14 +27,13 @@ public class CreateNewsStep {
 
     @Given("the page is fully loaded")
     public void the_page_is_fully_loaded() {
-        new CreateNewsPage(hooks.getDriver()).waitUntilPageLoaded();
+        createNewsPage.waitUntilPageLoaded();
     }
 
     @Then("^the (.+) block should be visible$")
     public void the_block_should_be_visible(String blockName) {
 
-        CreateNewsPage page = new CreateNewsPage(hooks.getDriver());
-        WebElement root = resolveBlockRoot(page, blockName);
+        WebElement root = resolveBlockRoot(createNewsPage, blockName);
         hooks.getSoftAssert().assertTrue(root != null && root.isDisplayed(),
                 blockName + " block is not visible");
     }
@@ -62,8 +62,7 @@ public class CreateNewsStep {
     @Then("^the (.+) label should be visible$")
     public void the_label_should_be_visible(String blockName) {
 
-        CreateNewsPage page = new CreateNewsPage(hooks.getDriver());
-        WebElement element = resolveRootBlockForLabel(page, blockName);
+        WebElement element = resolveRootBlockForLabel(createNewsPage, blockName);
         hooks.getSoftAssert().assertTrue(element != null &&
                         element.isDisplayed(),
                 blockName + " label is not visible");
@@ -84,8 +83,7 @@ public class CreateNewsStep {
     @Then("^the (.+) field info should be visible$")
     public void the_field_info_should_be_visible(String blockName) {
 
-        CreateNewsPage page = new CreateNewsPage(hooks.getDriver());
-        WebElement element = resolveRootBlockForFieldInfo(page, blockName);
+        WebElement element = resolveRootBlockForFieldInfo(createNewsPage, blockName);
         hooks.getSoftAssert().assertTrue(element != null &&
                         element.isDisplayed(),
                 blockName + " field info is not visible");
@@ -106,8 +104,7 @@ public class CreateNewsStep {
     @Then("^the (.+) input field should be visible$")
     public void the_input_field_should_be_visible(String blockName) {
 
-        CreateNewsPage page = new CreateNewsPage(hooks.getDriver());
-        WebElement element = resolveRootBlockForInputField(page, blockName);
+        WebElement element = resolveRootBlockForInputField(createNewsPage, blockName);
         hooks.getSoftAssert().assertTrue(element != null &&
                         element.isDisplayed(),
                 blockName + " input field is not visible");
@@ -127,39 +124,34 @@ public class CreateNewsStep {
 
     @Then("^the News tag should be selected$")
     public void the_News_tag_should_be_selected() {
-        hooks.getSoftAssert().assertTrue(new CreateNewsPage(hooks.getDriver())
-                .getNewsTagsComponent()
+        hooks.getSoftAssert().assertTrue(createNewsPage.getNewsTagsComponent()
                 .isTagSelected("News"),
                 "Tag News is not selected");
     }
 
     @When("the user unselects the News tag")
     public void the_user_unselects_the_News_tag() {
-        new CreateNewsPage(hooks.getDriver())
-                .getNewsTagsComponent()
+        createNewsPage.getNewsTagsComponent()
                 .unselectTag("News");
     }
 
     @Then("the News tag should not be selected")
     public void the_News_tag_should_not_be_selected() {
-        hooks.getSoftAssert().assertFalse(new CreateNewsPage(hooks.getDriver())
-                        .getNewsTagsComponent()
+        hooks.getSoftAssert().assertFalse(createNewsPage.getNewsTagsComponent()
                         .isTagSelected("News"),
                 "Tag News stayed selected");
     }
 
     @Then("the Image dropzone should be visible")
     public void the_image_dropzone_should_be_visible() {
-        hooks.getSoftAssert().assertTrue(new CreateNewsPage(hooks.getDriver())
-                        .getAddImageComponent()
+        hooks.getSoftAssert().assertTrue(createNewsPage.getAddImageComponent()
                         .getImageContainer().isDisplayed(),
                 "Image dropzone is not visible");
     }
 
     @Then("^the Content Editor field info should contain '([^']+)'$")
     public void the_content_editor_field_info_should_contain(String expectedText) {
-        hooks.getSoftAssert().assertTrue(new CreateNewsPage(hooks.getDriver())
-                .getContentEditor()
+        hooks.getSoftAssert().assertTrue(createNewsPage.getContentEditor()
                 .getFieldInfoElement()
                 .getText().contains(expectedText),
                 "Content Editor field info does not contain " + expectedText);
@@ -168,55 +160,52 @@ public class CreateNewsStep {
 
     @Then("the Author field should not be empty")
     public void the_author_field_should_not_be_empty() {
-        hooks.getSoftAssert().assertFalse(new CreateNewsPage(hooks.getDriver())
-                        .getAuthorOfNews().getText().trim().isEmpty(),
+        hooks.getSoftAssert().assertFalse(createNewsPage.getAuthorOfNews()
+                        .getText().trim().isEmpty(),
                 "the Author field is empty");
     }
 
     @Then("the Author field should match the logged-in username")
     public void the_author_field_should_match_the_logged_in_username() {
-        String username = new CreateNewsPage(hooks.getDriver())
-                        .getAuthorOfNews().getText().trim();
+        String username = createNewsPage.getAuthorOfNews().getText().trim();
         hooks.getSoftAssert().assertEquals(username, hooks.getTestValueProvider().getLsUserName(),
                 "Author field does not match the logged-in username");
     }
 
     @Then("the Author field should not be editable")
     public void the_author_field_should_not_be_editable() {
-        boolean isEditable = Boolean.parseBoolean(new CreateNewsPage(hooks.getDriver())
-                .getAuthorOfNews().
-                getAttribute("contenteditable"));
+        boolean isEditable = Boolean.parseBoolean(createNewsPage.getAuthorOfNews()
+                .getAttribute("contenteditable"));
         hooks.getSoftAssert().assertFalse(isEditable,
                 "the Author field is editable");
     }
 
     @Then("the Date field should be visible")
     public void the_date_field_should_be_visible() {
-        hooks.getSoftAssert().assertTrue(new CreateNewsPage(hooks.getDriver())
-                        .getActualDate().isDisplayed(),
+        hooks.getSoftAssert().assertTrue(createNewsPage.getActualDate()
+                        .isDisplayed(),
                 "the Date field is not visible");
     }
 
     @Then("the Date field should not be empty")
     public void the_date_field_should_not_be_empty() {
-        hooks.getSoftAssert().assertFalse(new CreateNewsPage(hooks.getDriver())
-                        .getActualDate().getText().trim().isEmpty(),
+        hooks.getSoftAssert().assertFalse(createNewsPage.getActualDate()
+                        .getText().trim().isEmpty(),
                 "the Date field is empty");
     }
 
     @Then("the Date field should not be editable")
     public void the_date_field_should_not_be_editable() {
-        boolean isEditable = Boolean.parseBoolean(new CreateNewsPage(hooks.getDriver())
-                .getActualDate().
-                getAttribute("contenteditable"));
+        boolean isEditable = Boolean.parseBoolean(createNewsPage.getActualDate()
+                .getAttribute("contenteditable"));
         hooks.getSoftAssert().assertFalse(isEditable,
                 "the Date field is editable");
     }
 
     @Then("the Date field should match the format 'MMMM d, yyyy'")
     public void the_date_field_should_match_the_format() {
-        boolean isValidFormat = new CreateNewsPage(hooks.getDriver())
-                .getActualDate().getText().trim()
+        boolean isValidFormat = createNewsPage.getActualDate()
+                .getText().trim()
                 .matches("^([A-Z][a-z]{2,}) ([1-9]|[12][0-9]|3[01]), \\d{4}$");
         hooks.getSoftAssert().assertTrue(isValidFormat,
                 "Date format should match 'MMMM d, yyyy'");
@@ -224,8 +213,7 @@ public class CreateNewsStep {
 
     @Then("^the Source field info should contain '([^']+)'$")
     public void the_source_field_info_should_contain(String expectedText) {
-        String actualText = new CreateNewsPage(hooks.getDriver())
-                        .getSourceInput()
+        String actualText = createNewsPage.getSourceInput()
                         .getFieldInfoElement()
                         .getText();
         hooks.getSoftAssert().assertTrue(
@@ -238,8 +226,7 @@ public class CreateNewsStep {
 
     @Then("the Cancel button should be visible")
     public void the_cancel_button_should_be_visible() {
-        hooks.getSoftAssert().assertTrue(new CreateNewsPage(hooks.getDriver())
-                        .getCreateNewsButtonsComponent()
+        hooks.getSoftAssert().assertTrue(createNewsPage.getCreateNewsButtonsComponent()
                         .getCancelButton()
                         .isDisplayed(),
                 "Cancel button is not visible");
@@ -247,7 +234,7 @@ public class CreateNewsStep {
 
     @Then("the Preview button should be visible")
     public void the_preview_button_should_be_visible() {
-        hooks.getSoftAssert().assertTrue(new CreateNewsPage(hooks.getDriver())
+        hooks.getSoftAssert().assertTrue(createNewsPage
                         .getCreateNewsButtonsComponent()
                         .getPreviewButton()
                         .isDisplayed(),
@@ -256,7 +243,7 @@ public class CreateNewsStep {
 
     @Then("the Publish button should be visible")
     public void the_publish_button_should_be_visible() {
-        hooks.getSoftAssert().assertTrue(new CreateNewsPage(hooks.getDriver())
+        hooks.getSoftAssert().assertTrue(createNewsPage
                         .getCreateNewsButtonsComponent()
                         .getPublishButton()
                         .isDisplayed(),
@@ -265,22 +252,19 @@ public class CreateNewsStep {
 
     @When("the user clicks on the Title field")
     public void the_user_clicks_on_the_title_field() {
-        new CreateNewsPage(hooks.getDriver())
-                .getTitleInput()
-                .clickOnField();
+        createNewsPage.getTitleInput().clickOnField();
     }
 
     @When("the user enters an empty title")
     public void the_user_enters_an_empty_title() {
-        new CreateNewsPage(hooks.getDriver())
-                .getTitleInput()
+        createNewsPage.getTitleInput()
                 .clickOnField()
                 .typeText("");
     }
 
     @Then("the Title field should be marked as invalid")
     public void the_title_field_should_be_marked_as_invalid() {
-        hooks.getSoftAssert().assertTrue(new CreateNewsPage(hooks.getDriver())
+        hooks.getSoftAssert().assertTrue(createNewsPage
                 .getTitleInput()
                 .isInvalidField(),
                 "Title field is not marked as invalid");
@@ -288,15 +272,15 @@ public class CreateNewsStep {
 
     @Then("the Publish button should be disabled in News creation page")
     public void the_publish_button_should_be_disabled() {
-        hooks.getSoftAssert().assertFalse(new CreateNewsPage(hooks.getDriver())
+        hooks.getSoftAssert().assertTrue(createNewsPage
                         .getCreateNewsButtonsComponent()
-                        .isPublishEnabled(),
+                        .isPublishDisabled(),
                 "Publish button is not disabled");
     }
 
     @Then("^the Title counter should display '([^']+)'$")
     public void the_title_counter_should_display_text(String text) {
-        hooks.getSoftAssert().assertTrue(new CreateNewsPage(hooks.getDriver())
+        hooks.getSoftAssert().assertTrue(createNewsPage
                         .getTitleInput()
                         .getFieldInfoText()
                         .contains(text),
@@ -306,14 +290,13 @@ public class CreateNewsStep {
     @When("the user enters a title longer than 170 characters")
     public void the_user_enters_a_title_longer_than_170_characters() {
         String longTitle = "A".repeat(172);
-        new CreateNewsPage(hooks.getDriver())
-                .getTitleInput()
+        createNewsPage.getTitleInput()
                 .typeText(longTitle);
     }
 
     @Then("the Title field value should not exceed 170 characters")
     public void the_title_field_value_should_not_exceed_170_characters() {
-        int actualLength = new CreateNewsPage(hooks.getDriver())
+        int actualLength = createNewsPage
                 .getTitleInput()
                 .getValue()
                 .length();
@@ -324,7 +307,7 @@ public class CreateNewsStep {
 
     @Then("the Title field should display a warning message")
     public void the_title_field_should_display_a_warning_message() {
-        hooks.getSoftAssert().assertTrue(new CreateNewsPage(hooks.getDriver())
+        hooks.getSoftAssert().assertTrue(createNewsPage
                 .getTitleInput()
                 .hasWarningFieldInfo(),
                 "Title warning message is not displayed");
@@ -332,7 +315,7 @@ public class CreateNewsStep {
 
     @Given("^the user enters a valid title '([^']+)'$")
     public void the_user_enters_a_valid_title(String validTitle) {
-        hooks.getSoftAssert().assertFalse(new CreateNewsPage(hooks.getDriver())
+        hooks.getSoftAssert().assertFalse(createNewsPage
                         .getTitleInput()
                         .clickOnField()
                         .typeText(validTitle)
@@ -342,7 +325,7 @@ public class CreateNewsStep {
 
     @Then("the Title field should not display a warning message")
     public void the_title_field_should_not_display_a_warning_message() {
-        hooks.getSoftAssert().assertFalse(new CreateNewsPage(hooks.getDriver())
+        hooks.getSoftAssert().assertFalse(createNewsPage
                         .getTitleInput()
                         .isInvalidField(),
                 "the Title field is displayed a warning message");
@@ -350,7 +333,7 @@ public class CreateNewsStep {
 
     @Then("the Main text field should remain empty")
     public void the_main_text_field_should_remain_empty() {
-        hooks.getSoftAssert().assertTrue(new CreateNewsPage(hooks.getDriver())
+        hooks.getSoftAssert().assertTrue(createNewsPage
                         .getContentEditor()
                         .getInputAreaText()
                         .trim().isEmpty(),
@@ -359,34 +342,34 @@ public class CreateNewsStep {
 
     @Then("the Publish button should remain disabled")
     public void the_publish_button_should_remain_disabled() {
-        hooks.getSoftAssert().assertFalse(new CreateNewsPage(hooks.getDriver())
+        hooks.getSoftAssert().assertTrue(createNewsPage
                         .getCreateNewsButtonsComponent()
-                        .isPublishEnabled(),
+                        .isPublishDisabled(),
                 "the Publish button is enable");
     }
 
     @Given("the user enters valid main text of at least 20 characters")
     public void the_user_enters_valid_main_text_of_at_least_20_characters() {
         String validMainText = "B".repeat(21);
-        new CreateNewsPage(hooks.getDriver())
-                        .getContentEditor()
+        createNewsPage.getContentEditor()
                         .clickOnMainText()
                         .typeText(validMainText);
     }
 
     @Given("the user selects the News tag")
     public void the_user_selects_the_news_tag() {
-        new CreateNewsPage(hooks.getDriver())
-                        .getNewsTagsComponent()
+        createNewsPage.getNewsTagsComponent()
                         .selectTag("News");
     }
 
     @Then("the Publish button should be enabled")
     public void the_publish_button_should_be_enabled() {
-        hooks.getSoftAssert().assertTrue(new CreateNewsPage(hooks.getDriver())
+        hooks.getSoftAssert().assertTrue(createNewsPage
                         .getCreateNewsButtonsComponent()
                         .isPublishEnabled(),
                 "the Publish button is not enabled");
     }
+
+
 
 }
