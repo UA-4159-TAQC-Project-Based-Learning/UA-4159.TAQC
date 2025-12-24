6aa769ccb701b9b046b0b64c5175161ecd93f0a6
maryyna-lobatiuk <maryna.lobatiuk@gmail.com>
2025-12-24 22:07:30 +0100

    add FileClient.java, FileControllerTest.java with tests for sigle file upload and delete

--- src/main/java/com/greencity/api/models/ownsecurity/SignInResponse.java
+++ src/main/java/com/greencity/api/models/ownsecurity/SignInResponse.java
@@ -1,9 +1,13 @@
 package com.greencity.api.models.ownsecurity;
 
+import lombok.Getter;
+
 public class SignInResponse {
     public Integer userId;
+    @Getter
     public String accessToken;
     public String refreshToken;
     public String name;
     public Boolean ownRegistrations;
+
 }
